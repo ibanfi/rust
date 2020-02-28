18a7ee977202afbd789c5df2d156370eac69fed5
Bánfi István <ibanfi@gmail.com>
2020-02-28 15:10:15 +0100

    add git2

--- res/bootstrap.sh
+++ res/bootstrap.sh
@@ -2,6 +2,8 @@
 
 # Set prompt
 echo "export PS1=\"\[\e[1;91m\]\h:\W $\[\e[0m\] \"" >> .bash_profile
+echo "eval `ssh-agent`" >> .bash_profile
+echo "ssh-add ~/.ssh/forte/id_rsa" >> .bash_profile
 
 # Set LANG environments
 echo "LANG=en_US.utf-8" > /etc/environment
@@ -17,8 +19,12 @@ curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.r
 # Add nodejs repository
 curl -sL https://rpm.nodesource.com/setup_10.x | bash
 
+# Add git repo
+yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+yum remove git*
+
 # Install packages
-yum install -y gcc-c++ make libtool net-tools git device-mapper-persistent-data lvm2 yarn nodejs docker-ce docker-ce-cli openssl-devel unzip postgresql-devel epel-release redis lsof golang gmp-devel python-pip
+yum install -y gcc-c++ make libtool net-tools git2u-all device-mapper-persistent-data lvm2 yarn nodejs docker-ce docker-ce-cli openssl-devel unzip postgresql-devel epel-release redis lsof golang gmp-devel python-pip
 
 # Start docker
 systemctl enable docker
@@ -33,3 +39,10 @@ chmod +x /usr/local/bin/docker-compose
 
 # Install AWS CLI
 pip install awscli --upgrade --user
+
+# Install helm 2
+curl -sL https://get.helm.sh/helm-v2.16.3-linux-amd64.tar.gz | tar -zx
+tar -zxf helm.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
+rm helm.tar.gz
+rm -rf linux-amd64
